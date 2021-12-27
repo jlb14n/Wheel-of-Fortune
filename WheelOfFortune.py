5951988b2a716ce945c15bf484bc81584552bd4f
jlb14n <jlb14n@gmail.com>
2021-12-27 00:09:53 -0500

    Added stretch goals: One-Million, Jackpot, and Mystery wedges added

--- WheelOfFortune.py
+++ WheelOfFortune.py
@@ -78,7 +78,7 @@ def vowels_left(word,guess_his):
     return not remainder.isdisjoint(vowel)
 #------------------------------------------------------------------------------------------------------------------------------------------
 #Initialization
-wheel=[600,500,300,500,800,550,400,300,900,500,300,900,"Bankrupt",600,400,300,"Lose A Turn",800,350,450,700,300,600,600]
+wheel=[600,500,300,500,800,550,400,300,900,500,300,900,"Bankrupt",600,400,300,"Lose A Turn",800,350,450,700,"Mystery",600,"One Million"]
 final_prize=100000 #Stretch Goal: Change to be randomly chosen from a list
 num_players=3
 num_rounds=3 #Including the final round
@@ -87,7 +87,7 @@ round=0 #Tracks round number, starting from 0
 guess_his=set()
 player=[] #Player list of dictionaries
 for i in range(0,num_players):
-    player.append({"name":"", "bank":0, "round_bank":0})
+    player.append({"name":"", "bank":0, "round_bank":0, "round_prize":0, "prize":0})
 active=random.randint(0,2) #Tracks the active player, randomizes who the first player will be
 word=choose_words("words_list.txt",num_rounds) #This decides what the three words are going to be this game! Stretch Goal: Import in real prompts
 #------------------------------------------------------------------------------------------------------------------------------------------
@@ -99,10 +99,14 @@ print("\nWelcome to Wheel of Fortune!\n")
 for i in  range(0,num_players): #Inputs player names
     player[i].update({"name":input("What is the name of Player {0}?: ".format(i+1))})
 while round<num_rounds-1:
+    if round>0: #Adding jackpot segment and replacing the mystery wedges after round 1
+        wheel[22]=5000
+        wheel[21]="Mystery"
     input("\nIt is currently {0}'s turn! (Enter to continue) ".format(player[active]["name"]))
     display_word(word[round],guess_his)
     while True: #Player's Turn
-        if y_n_input("Would you like to guess the word? (y/n): "): #Guess word
+        #Guess Word
+        if y_n_input("Would you like to guess the word? (y/n): "):
             guess=input("Guess the word: ").lower()
             if guess==word[round]: #Round ending!
                 print("That is correct! The round is over!\n")
@@ -110,23 +114,28 @@ while round<num_rounds-1:
                     player[active]["bank"]+=player[active]["round_bank"]
                 else:
                     player[active]["bank"]+=min_round_prize #Minimum price won is $1000
+                player[active]["prize"]+=player[active]["round_prize"]
                 round+=1
                 guess_his=set()
                 for i in range(0,num_players):
-                    player[i].update({"round_bank":0})
-                    print("{0} has ${1}".format(player[i]["name"],player[i]["bank"]))
+                    player[i].update({"round_bank":0, "round_prize":0})
+                    print("{0} has ${1} (with ${2} in prizes)!".format(player[i]["name"],player[i]["bank"],player[i]["prize"]))
                 print("\nWe will return for round {0} after THIS commercial break...\n".format(round+1))
                 break
             else:
                 print("That is not correct.")
                 break
-        else: #Spin wheel
+        #Spin Wheel
+        else: 
             input("\nSPIN THE WHEEL!\n") #This is to give a better user experience to enter something to spin the wheel.
             wheel_active=wheel[random.randint(0,len(wheel)-1)]
+            if wheel_active=="One Million": #Changes the "One Million" wedge into a one-third change of landing on Bankrupt
+                if random.randint(0,2)!=1:
+                    wheel_active="Bankrupt"
             print("The wheel landed on: {0}".format(wheel_active))
             if wheel_active=="Bankrupt":
                 print("Oh No!")
-                player[active].update({"round_bank":0})
+                player[active].update({"round_bank":0,"round_prize":0})
                 break
             elif wheel_active=="Lose A Turn":
                 print("Tough luck!")
@@ -136,10 +145,27 @@ while round<num_rounds-1:
                 guess_his.add(guess)
                 num_letters=letter_check(word[round],guess,guess_his)
                 if num_letters!=0:
-                    player[active]["round_bank"]+=num_letters*wheel_active
+                    if wheel_active=="One Million":
+                        player[active].update({"round_prize":1000000})
+                    elif wheel_active=="Mystery": #It seems like they only get to go bankrupt or get the cash prize after they guess a letter correct.
+                        if y_n_input("Would you like to take the 50/50 for $10,000 instead of $1000 per letter? (y/n): "):
+                            if random.randint(0,1)==0:
+                                print("It's $10,000!")
+                                player[active]["round_bank"]+=10000 #Not sure if they can use this money to buy vowels or not... assuming they can for now
+                            else:
+                                print("It's Bankrupt!")
+                                player[active].update({"round_bank":0,"round_prize":0})
+                                break
+                            wheel[21]=600 #Now that the mystery wedge has been picked up, it is replaced with a $600 wedge
+                        else:
+                            player[active]["round_bank"]+=num_letters*1000
+                    elif wheel_active>=5000: #If they hit the jackpot!
+                        player[active]["round_bank"]+=wheel_active
+                    else:
+                        player[active]["round_bank"]+=num_letters*wheel_active
                     display_word(word[round],guess_his)
                     while True: #Buy a vowel loop
-                        print("You currently have ${0} (this round)!".format(player[active]["round_bank"]))         
+                        print("You currently have ${0} this round (with ${1} in prizes)!".format(player[active]["round_bank"],player[active]["round_prize"]))         
                         if vowels_left(word[round],guess_his):
                             if player[active]["round_bank"]>=250:
                                 if y_n_input("Would you like to buy a vowel? (y/n): "):
@@ -203,6 +229,7 @@ else: #Final Round
         player[active]["bank"]+=final_prize
     else:
         print("That is not correct. You, unfortunately, missed out on ${0}.\n".format(final_prize))
+    player[active]["bank"]+=player[active]["prize"] #If they got the million dollar wedge and finished "as a champion". I'm not sure if that means they had to get the final round correct or not... if so, this should be 0within the previous if statement instead
 for i in range(0,num_players):
     print("{0} has ended with ${1}".format(player[i]["name"],player[i]["bank"]))
 print("Thanks for playing!")
